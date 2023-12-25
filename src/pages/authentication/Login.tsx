36e86ecbbc02b96d9d26d7c911d0c603ee1bbbe3
Candra0x6 <kurniawancandram@gmail.com>
2023-12-26 00:01:20 +0700

    style : add ui for login page

---
+++ src/pages/authentication/Login.tsx
@@ -0,0 +1,82 @@
+import { Box, Button, FormControl, IconButton, Input } from "@chakra-ui/react";
+import WestIcon from "@mui/icons-material/West";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LockIcon from "@mui/icons-material/Lock";
+import { Link } from "react-router-dom";
+function Login() {
+  return (
+    <div className="flex justify-center font-poppins">
+      <div className="container mx-2">
+        <div className="flex flex-col">
+          <div className="mt-5">
+            <Link to={`/welcome`}>
+              <IconButton
+                aria-label="Backk"
+                bgColor="transparent"
+                icon={<WestIcon fontSize="large" />}
+              />
+            </Link>
+          </div>
+          <div className="">
+            <h1 className="font-semibold text-[35px] py-7">
+              Ayy, <br /> Welcome <br /> Back
+            </h1>
+          </div>
+          <div className="font-poppins">
+            <FormControl fontFamily="">
+              <Box display="flex" alignItems="center">
+                <Input
+                  border="2px"
+                  borderColor="#AAC3CC"
+                  bgColor="#E2F4FA"
+                  className="bg-[#AAC3CC]"
+                  rounded="full"
+                  type="email"
+                  textColor="#4E4E4E"
+                  placeholder={`        Email`}
+                />
+                <MailOutlineIcon className="absolute left-4 text-[#4e4e4eb0]" />
+              </Box>
+              <Box display="flex" marginY="15px" alignItems="center">
+                <Input
+                  border="2px"
+                  borderColor="#AAC3CC"
+                  bgColor="#E2F4FA"
+                  className="bg-[#AAC3CC]"
+                  rounded="full"
+                  type="email"
+                  textColor="#4E4E4E"
+                  placeholder={`        Password`}
+                />
+                <LockIcon className="absolute left-4 text-[#4e4e4eb0]" />
+              </Box>
+              <Button
+                variant="link"
+                display="flex"
+                className="text-normal"
+                w="full"
+                fontWeight="normal"
+              >
+                Forget Password?
+              </Button>
+              <Button
+                w="full"
+                bgColor="#373739"
+                border="2px"
+                borderColor="#424E52"
+                marginTop="40px"
+                textColor="white"
+                rounded="full"
+                fontWeight="normal"
+              >
+                Login
+              </Button>
+            </FormControl>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
