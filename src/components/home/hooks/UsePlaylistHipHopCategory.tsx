2bbba9bff9ec2c99df512b10bf85a3aa2db74759
Candra0x6 <kurniawancandram@gmail.com>
2023-12-30 00:51:20 +0700

    add : fetch data for hiphop category component

---
+++ src/components/home/hooks/UsePlaylistHipHopCategory.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
+import { Playlist } from "..";
+
+function UsePlaylistHipHopCategory() {
+  const getAT = localStorage.getItem("access_token");
+  const [hipHopPlaylist, setHipHopPlaylist] = useState<Playlist[]>([]);
+  const getPlaylistByHipHopCategory = async () => {
+    try {
+      const fetch = await axios.get(
+        `${
+          import.meta.env.VITE_REACT_APP_BASE_URL
+        }/browse/categories/0JQ5DAqbMKFQ00XGBls6ym/playlists?limit=10
+        `,
+        {
+          headers: {
+            Authorization: `Bearer ${getAT}`,
+          },
+        }
+      );
+      const hiphopData = fetch.data.playlists.items;
+      setHipHopPlaylist(hiphopData);
+    } catch (e) {}
+  };
+
+  console.log(localStorage);
+  return {
+    getPlaylistByHipHopCategory,
+    hipHopPlaylist,
+  };
+}
+
+export default UsePlaylistHipHopCategory;
