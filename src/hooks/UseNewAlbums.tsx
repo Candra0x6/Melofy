f45b8f8566d211de2b30150b8c973195bc66748e
Candra0x6 <kurniawancandram@gmail.com>
2024-01-10 21:19:54 +0700

    add : hook fetch album

---
+++ src/hooks/UseNewAlbums.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { NewAlbums } from ".";
+import { useState } from "react";
+
+function UseNewAlbums() {
+  const getAT = localStorage.getItem("access_token");
+  const [newAlbums, setNewAlbums] = useState<NewAlbums[]>([]);
+  const getNewReleaseAlbum = async () => {
+    try {
+      const fetch = await axios.get(
+        `${
+          import.meta.env.VITE_REACT_APP_BASE_URL
+        }/browse/new-releases?limit=10`,
+        {
+          headers: {
+            Authorization: `Bearer ${getAT}`,
+          },
+        }
+      );
+
+      const data = fetch.data.albums.items;
+      setNewAlbums(data);
+    } catch (e) {}
+  };
+
+  const getPlaylistByCategory = async () => {
+    try {
+      const fetch = await axios.get(
+        `${import.meta.env.VITE_REACT_APP_BASE_URL}/me`,
+        {
+          headers: {
+            Authorization: `Bearer ${getAT}`,
+          },
+        }
+      );
+      console.log(fetch);
+    } catch (e) {}
+  };
+  return {
+    newAlbums,
+    getNewReleaseAlbum,
+    getPlaylistByCategory,
+  };
+}
+
+export default UseNewAlbums;
