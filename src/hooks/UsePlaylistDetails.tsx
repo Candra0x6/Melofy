35c5285b86259d322f309f0c81006203ce798bad
Candra0x6 <kurniawancandram@gmail.com>
2024-01-13 16:01:27 +0700

    add : hooks fetch api

---
+++ src/hooks/UsePlaylistDetails.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Playlist } from ".";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+function UsePlaylistDetails() {
+  const [playlistDetail, setPlaylistDetail] = useState<Playlist>();
+  const getAt = localStorage.getItem("access_token");
+  const { playlist_id } = useParams();
+  //   get playlist detail from api
+  const getPlaylistDetails = async () => {
+    try {
+      const playlist = await axios.get(
+        `${import.meta.env.VITE_REACT_APP_BASE_URL}/playlists/${playlist_id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${getAt}`,
+          },
+        }
+      );
+      const result = playlist.data;
+      setPlaylistDetail(result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return {
+    playlistDetail,
+    getPlaylistDetails,
+  };
+}
+
+export default UsePlaylistDetails;
