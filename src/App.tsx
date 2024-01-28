67af6b4af3ae82192ef666d0a12d531b9e8cd59c
Candra0x6 <kurniawancandram@gmail.com>
2024-01-28 15:42:04 +0700

    add : route page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Signup from "./pages/authentication/Signup";
 import PlaylistDetail from "./pages/playlist/[playlist_id]/PlaylistDetail";
 import TrackPlayer from "./pages/track/[track_id]/TrackPlayer";
 import SearchPage from "./pages/search/SearchPage";
+import ArtistDetails from "./pages/artist/[artist_id]/ArtistDetails";
+import AlbumDetail from "./pages/album/[album_id]/AlbumDetail";
 
 function App() {
   const ac = localStorage.getItem("access_token");
@@ -25,6 +27,8 @@ function App() {
         <Route path="/search" element={<SearchPage />} />
         <Route path="/playlist/:playlist_id" element={<PlaylistDetail />} />
         <Route path="/track/:track_id" element={<TrackPlayer />} />
+        <Route path="/artist/:artist_id" element={<ArtistDetails />} />
+        <Route path="/album/:album_id" element={<AlbumDetail />} />
         <Route path="*" element={<Cover />} />
       </Routes>
     </Router>
