b789202663799ce241010cab5a6a0486b7fe022c
Candra0x6 <kurniawancandram@gmail.com>
2024-02-04 13:54:35 +0700

    add : type issidebar

--- src/components/element/type/index.ts
+++ src/components/element/type/index.ts
@@ -2,3 +2,7 @@ export interface isModalActive {
   visibleModalSearch: boolean;
   setVisibleModalSearch: React.Dispatch<React.SetStateAction<boolean>>;
 }
+export interface isSideBarActive {
+  visibleSideBar: boolean;
+  setVisibleSideBar: React.Dispatch<React.SetStateAction<boolean>>;
+}
