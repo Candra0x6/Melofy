161e7458632be440f0465905b73f96869a0a732e
Candra0x6 <kurniawancandram@gmail.com>
2024-02-17 12:20:06 +0700

    style : miss position

--- src/components/UserPageTitle.tsx
+++ src/components/UserPageTitle.tsx
@@ -4,12 +4,16 @@ interface TitleDeclare {
 }
 function UserPageTitle({ title }: TitleDeclare) {
   return (
-    <div className="flex w-full items-center mt-4">
-      <ButtonBack />
-      <div className="text-center w-full font-semibold tracking-widest">
-        <h1 className="text-center">{title}</h1>
+    <>
+      <div className="">
+        <ButtonBack />
       </div>
-    </div>
+      <div className="flex w-full items-start py-4">
+        <div className="text-center w-full font-semibold tracking-widest">
+          <h1 className="text-center">{title}</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
