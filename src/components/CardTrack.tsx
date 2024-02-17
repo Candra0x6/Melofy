41686ef6ad1d15e53787a8f81dad7138eefa530c
Candra0x6 <kurniawancandram@gmail.com>
2024-02-17 12:17:19 +0700

    fix : miss paramater truncate text

--- src/components/CardTrack.tsx
+++ src/components/CardTrack.tsx
@@ -12,13 +12,13 @@ interface CardValue {
 function CardTrack({ data, title }: CardValue) {
   const { truncateText } = UseTruncateText();
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col z-10 mx-2">
       <h1 className="font-bold text-xl">{title}</h1>
       <div className="flex flex-col gap-y-3 mt-5">
         {data &&
           data.map((track, id: number) => (
-            <Link className="z-10" to={`/track/${track.id}`} key={id}>
-              <div className="flex justify-between z-10 items-center w-full">
+            <Link className="-z-0" to={`/track/${track.id}`} key={id}>
+              <div className="flex justify-between z-0 items-center w-full">
                 <div className="flex gap-x-3 items-center">
                   <div className="flex">
                     <div className="h-[60px] w-[60px] aspect-square">
@@ -39,7 +39,7 @@ function CardTrack({ data, title }: CardValue) {
                   <div className="tracking-wider overflow-hidden">
                     <div className="flex">
                       <h1 className="font-semibold text-[14px] text-black">
-                        {truncateText(track && track.name) || "Title"}
+                        {truncateText(track && track.name, 30) || "Title"}
                       </h1>
                     </div>
                     <h2 className="font-medium text-[#838383] text-[10px]">
