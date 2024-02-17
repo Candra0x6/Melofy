8bfdc234366b036ed1986736212e8bfe522dd635
Candra0x6 <kurniawancandram@gmail.com>
2024-02-17 12:26:39 +0700

    add : get user id from local storage

--- src/hooks/LocalStorageHandle.tsx
+++ src/hooks/LocalStorageHandle.tsx
@@ -1,7 +1,12 @@
 function LocalStorageHandle() {
   const getAccessToken = localStorage.getItem("access_token");
+  const getUserID = localStorage.getItem("user_id");
+
+  const accessToken = (): string | null => localStorage.getItem("access_token");
   return {
     getAccessToken,
+    getUserID,
+    accessToken,
   };
 }
 
