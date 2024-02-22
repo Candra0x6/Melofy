437fadaf95d4b29c8ccb59b0398843ba530ef2b3
Candra0x6 <kurniawancandram@gmail.com>
2024-02-23 03:49:00 +0700

    import : page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import TrackPlayer from "./pages/track/[track_id]/TrackPlayer";
 import SearchPage from "./pages/search/SearchPage";
 import ArtistDetails from "./pages/artist/[artist_id]/ArtistDetails";
 import AlbumDetail from "./pages/album/[album_id]/AlbumDetail";
+import CategoryDetail from "./pages/category/[category_id]/CategoryDetail";
+import UserProfile from "./pages/User/Profile/UserProfile";
+import FollowedArtist from "./pages/User/Follow/FollowedArtist";
+import UserLibrary from "./pages/User/Library/UserLibrary";
 
 function App() {
   const ac = localStorage.getItem("access_token");
@@ -29,6 +33,10 @@ function App() {
         <Route path="/track/:track_id" element={<TrackPlayer />} />
         <Route path="/artist/:artist_id" element={<ArtistDetails />} />
         <Route path="/album/:album_id" element={<AlbumDetail />} />
+        <Route path="/category/:category_id" element={<CategoryDetail />} />
+        <Route path="/user/profile" element={<UserProfile />} />
+        <Route path="/user/follow" element={<FollowedArtist />} />
+        <Route path="/user/library" element={<UserLibrary />} />
         <Route path="*" element={<Cover />} />
       </Routes>
     </Router>
