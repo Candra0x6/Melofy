fd5ae06bf9def0e67f277e161f6707c32fe612f4
Candra0x6 <kurniawancandram@gmail.com>
2024-02-24 23:03:50 +0700

    style : change to child component

--- src/components/home/components/NewReleases.tsx
+++ src/components/home/components/NewReleases.tsx
@@ -1,16 +1,6 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Grid,
-  Heading,
-  Image,
-  Text,
-} from "@chakra-ui/react";
 import { useEffect } from "react";
 import UseNewAlbums from "../../../hooks/UseNewAlbums";
-import { ArrowForwardIos } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import DiscoverCardPlaylist from "../../DiscoverCardPlaylist";
 export default function NewReleases() {
   const { getNewReleaseAlbum, newAlbums, getPlaylistByCategory } =
     UseNewAlbums();
@@ -19,50 +9,6 @@ export default function NewReleases() {
     getPlaylistByCategory();
     getNewReleaseAlbum();
   }, []);
-  return (
-    <Flex display="inline-block" w="full">
-      <Flex justifyContent="space-between" alignItems="center" pb="10px">
-        <h1 className="font-medium font-krona text-[12px] text-center">
-          <span className=" before:absolute before:inset-2 before:left-0 before:w-full before:h-1/2 before:bg-[#B1DEEC] relative inline-block">
-            <span className="relative text-black ">New Albums</span>
-          </span>
-        </h1>
 
-        <button className="text-[10px] text-[#838383] flex items-center">
-          See All
-          <ArrowForwardIos sx={{ fontSize: "10px" }} />
-        </button>
-      </Flex>
-      <Flex className="overflow-x-auto no-scrollbar">
-        <Grid row="auto" gap={4} display="flex" w="full">
-          {newAlbums.length > 0 &&
-            newAlbums.map((val, key) => (
-              <Link to={`/${val.type}/${val.id}`} key={key}>
-                <Box
-                  key={key}
-                  display="flex"
-                  flexDirection="column"
-                  justifyContent="start"
-                >
-                  <Image
-                    src={val.images[0].url}
-                    rounded="10px"
-                    maxW="8rem"
-                    maxH="8rem"
-                  />
-                  <Heading fontWeight="semibold" fontSize="14px">
-                    {val.name}
-                  </Heading>
-                  {val.artists.map((artist) => (
-                    <Text key={artist.id} fontSize="10px">
-                      {artist.name}
-                    </Text>
-                  ))}
-                </Box>
-              </Link>
-            ))}
-        </Grid>
-      </Flex>
-    </Flex>
-  );
+  return <DiscoverCardPlaylist data={newAlbums} title="New Albums" />;
 }
