956b53ed0c6bca7eb23527eface09cb17da83b6e
Candra0x6 <kurniawancandram@gmail.com>
2024-02-28 08:27:59 +0700

    style : compare to component

--- src/pages/category/[category_id]/CategoryDetail.tsx
+++ src/pages/category/[category_id]/CategoryDetail.tsx
@@ -1,32 +1,21 @@
-import { IconButton } from "@chakra-ui/react";
 import {
   UseCategoryByID,
   UseCategoryPlaylist,
 } from "../../../hooks/UseCategory";
-import { MoreVert, West } from "@mui/icons-material";
-import { Link } from "react-router-dom";
-import UseTruncateText from "../../../hooks/UseTruncateText";
+
+import ButtonBack from "../../../components/ButtonBack";
+import CardPlaylist from "../../../components/CardPlaylist";
 
 function CategoryDetail() {
   const { categoryPlaylist } = UseCategoryPlaylist();
   const { categoryDetail } = UseCategoryByID();
-  const { truncateText } = UseTruncateText();
 
   return (
     <div className="flex justify-center overflow-hidden relative">
       <div className="container">
         <div className="flex flex-col h-[280px] w-full relative">
-          <div className="flex w-full justify-start ml-2 ">
-            <IconButton
-              position="absolute"
-              top="2"
-              onClick={() => window.history.back()}
-              aria-label="Backk"
-              bgColor="#F7FCFE"
-              zIndex="10"
-              rounded="full"
-              icon={<West sx={{ fontSize: 28 }} />}
-            />
+          <div className="flex w-full">
+            <ButtonBack />
           </div>
           <div className="flex flex-col absolute h-full w-full justify-center items-center">
             <h1 className="text-gray-100 font-poppins font-semibold text-2xl w-full z-20 mb-20 text-center">
@@ -49,40 +38,10 @@ function CategoryDetail() {
             />
           </div>
         </div>
-        <div className="flex flex-col gap-y-5 mx-2 -mt-10">
-          <h1>Playlist</h1>
-          {categoryPlaylist &&
-            categoryPlaylist.playlists &&
-            categoryPlaylist.playlists.items &&
-            categoryPlaylist.playlists.items.map((track, id) => (
-              <Link className="z-10" to={`/${track.type}/${track.id}`} key={id}>
-                <div className="flex justify-between z-10 items-center w-full">
-                  <div className="flex gap-x-3 items-center">
-                    <div className="h-[60px] w-[60px] aspect-square">
-                      <img
-                        src={track.images[0].url}
-                        className=" w-full h-full rounded-md"
-                        alt={`Track${id}`}
-                      />
-                    </div>
-                    <div className="tracking-wider">
-                      <h1 className="font-semibold text-[15px] text-black">
-                        {truncateText(track.name)}
-                      </h1>
-                      <h2 className="font-medium text-[#838383] text-[12px]">
-                        {truncateText(track.description || "Description")}
-                      </h2>
-                    </div>
-                  </div>
-                  <IconButton
-                    aria-label="more icon"
-                    bg="transparent"
-                    icon={<MoreVert sx={{ fontSize: "24px" }} />}
-                  />
-                </div>
-              </Link>
-            ))}
-        </div>
+        <CardPlaylist
+          data={categoryPlaylist?.playlists.items}
+          title="Playlist"
+        />
       </div>
     </div>
   );
