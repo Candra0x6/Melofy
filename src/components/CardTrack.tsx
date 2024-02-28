04650a30f73cf63357076dfb9ff4f7807fd52d76
Candra0x6 <kurniawancandram@gmail.com>
2024-02-28 08:22:49 +0700

    style : change component

--- src/components/CardTrack.tsx
+++ src/components/CardTrack.tsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import { Track } from "../hooks";
 import UseTruncateText from "../hooks/UseTruncateText";
-import { IconButton } from "@chakra-ui/react";
-import { MoreVert } from "@mui/icons-material";
+import UseAddItemToPlaylist from "../hooks/UseAddItemToPlaylist";
+import TrackInteract from "./TrackInteract";
 
 interface CardValue {
   data: Track[] | undefined;
-  title: string | undefined;
+  title: string;
 }
 
 function CardTrack({ data, title }: CardValue) {
+  const { addTrack } = UseAddItemToPlaylist();
   const { truncateText } = UseTruncateText();
   return (
     <div className="flex flex-col z-10 mx-2">
@@ -17,8 +18,8 @@ function CardTrack({ data, title }: CardValue) {
       <div className="flex flex-col gap-y-3 mt-5">
         {data &&
           data.map((track, id: number) => (
-            <Link className="-z-0" to={`/track/${track.id}`} key={id}>
-              <div className="flex justify-between z-0 items-center w-full">
+            <div className="flex justify-between items-center w-full">
+              <Link className="z-0" to={`/track/${track.id}`} key={id}>
                 <div className="flex gap-x-3 items-center">
                   <div className="flex">
                     <div className="h-[60px] w-[60px] aspect-square">
@@ -35,7 +36,6 @@ function CardTrack({ data, title }: CardValue) {
                       />
                     </div>
                   </div>
-
                   <div className="tracking-wider overflow-hidden">
                     <div className="flex">
                       <h1 className="font-semibold text-[14px] text-black">
@@ -47,13 +47,9 @@ function CardTrack({ data, title }: CardValue) {
                     </h2>
                   </div>
                 </div>
-                <IconButton
-                  aria-label="more icon"
-                  bg="transparent"
-                  icon={<MoreVert sx={{ fontSize: "24px" }} />}
-                />
-              </div>
-            </Link>
+              </Link>
+              <TrackInteract />
+            </div>
           ))}
       </div>
     </div>
