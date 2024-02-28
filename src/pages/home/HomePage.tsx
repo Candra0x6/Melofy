70822f7fef999ea9a91196c654291cac69e96277
Candra0x6 <kurniawancandram@gmail.com>
2024-02-28 08:28:28 +0700

    style : ui home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -8,10 +8,10 @@ import Slider from "../../components/home/components/Slider";
 function HomePage() {
   return (
     <main>
-      <div className="flex flex-col justify-center items-center mx-1 sticky overflow-hidden bg-[#F7FCFE]">
+      <div className="flex flex-col justify-center items-center sticky overflow-hidden bg-[#F7FCFE]">
         <div className="container">
           <Slider />
-          <div className="overflow-hidden relative w-full flex flex-col gap-y-5 ">
+          <div className="overflow-hidden relative w-full flex flex-col gap-y-5">
             <RecomendedPlaylist />
             <NewReleases />
             <HipHopPlaylist />
